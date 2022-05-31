37421ac244e34c7abf6cbcfeda39834380ade645
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-05-31 10:46:24 -0300

    feat: retornar informacoes da api

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -4,6 +4,7 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
+        public int Id { get; internal set; }
         [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
         [Required(ErrorMessage = "O campo diretor é obrigatório")]
