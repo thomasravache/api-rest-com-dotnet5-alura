ae6ce6bc2b6d70b7e30f87efec177c9b0b55169d
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-05-31 13:16:33 -0300

    feat: escrever e ler dados do banco de dados

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesAPI.Models;
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,15 +11,18 @@ namespace FilmesAPI.Controllers
     [Route("[controller]")]
     public class FilmeController : ControllerBase
     {
-        private static List<Filme> filmes = new List<Filme>();
-        private static int id = 1;
+        private FilmeContext _context;
+
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
 
         [HttpPost]
         public IActionResult AdicionaFilme([FromBody] Filme filme)
         {
-            filme.Id = id++;
-            filmes.Add(filme);
-
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
             // definir action para recuperar o recurso criado (no caso o filme, para recuperar só utilizar o RecuperarFilmesPorId com o Id do filme
             return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
         }
@@ -26,13 +30,13 @@ namespace FilmesAPI.Controllers
         [HttpGet]
         public IActionResult RecuperaFilmes()
         {
-            return Ok(filmes);
+            return Ok(_context.Filmes);
         }
 
         [HttpGet("{id}")]
         public IActionResult RecuperaFilmesPorId(int id)
         {
-           Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+           Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
             if (filme != null)
             {
