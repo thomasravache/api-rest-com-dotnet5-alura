3124f695281f0a227d53a3405956fc2cd238a3fb
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-05-31 10:37:07 -0300

    feat: validar parametros recebidos e adicionar restricoes

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,10 +1,16 @@
-﻿namespace FilmesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
 {
     public class Filme
     {
+        [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatório")]
         public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero não pode ultrapassar 30 caracteres")]
         public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
         public int Duracao { get; set; }
     }
 }
